ccabcc03b77ff44c6fed5518d03712d6db771341
parthiv <parmarparthiv@gmail.com>
2024-04-04 22:05:11 +0530

    adding cors origin

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import router from "./routes";
 import dotenv from "dotenv";
 dotenv.config();
@@ -6,6 +7,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors({}));
 app.use(express.json());
 
 app.use("/api", router);
